81403fb80c40c0aa207c1c638e96d12aab51fead
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-07 23:35:28 +0300

    add header

---
+++ berezovskoye-frontend/src/components/Header.tsx
@@ -0,0 +1,63 @@
+import Image from 'next/image'
+import {HeaderItems} from "@/items/HeaderItems";
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+    const headerItems = HeaderItems();
+
+    return (
+        <header className="sticky top-0 flex h-[70px] items-center justify-between border-b bg-back-bars px-8">
+            <Image
+                alt={"logo"}
+                src={"/logo.svg"}
+                width={200}
+                height={55}
+            />
+
+            <div className="flex items-center gap-4">
+                {
+                    headerItems.map((item, index) => (
+                        <HeaderNavItem
+                            key={index}
+                            label={item.name}
+                            iconPath={item.imageSrc}
+                            href={item.href}
+                        />
+                    ))
+                }
+            </div>
+
+            <HeaderNavItem
+                label={"О нас"}
+                iconPath={"/about-us.svg"}
+                href={"/about"}
+            />
+        </header>
+    );
+}
+
+interface HeaderNavItemProps {
+    label: string;
+    iconPath: string;
+    href: string;
+}
+
+export const HeaderNavItem: React.FC<HeaderNavItemProps> = ({label, iconPath, href}) => {
+    return (
+        <div className="flex items-center gap-1 text-white">
+            <Link
+                href={href}
+                className="flex flex-row items-center space-x-2 px-2 py-1.5 text-sm"
+            >
+                <Image
+                    src={iconPath}
+                    width={30}
+                    height={30}
+                    alt=""
+                />
+                <span>{label}</span>
+            </Link>
+        </div>
+    );
+};
---
+++ berezovskoye-frontend/src/items/HeaderItems.tsx
@@ -0,0 +1,24 @@
+export const HeaderItems = () => {
+    return [
+        {
+            name: 'Товары',
+            href: '/products',
+            imageSrc: '/products.svg',
+        },
+        {
+            name: 'Новости',
+            href: '/news',
+            imageSrc: '/news.svg',
+        },
+        {
+            name: 'Услуги',
+            href: '/services',
+            imageSrc: '/services.svg',
+        },
+        {
+            name: 'Контакты',
+            href: '/contacts',
+            imageSrc: '/contacts.svg',
+        },
+    ];
+};
