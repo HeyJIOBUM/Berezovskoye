890c4eca0e60a23e10ce2fa3a19e26238fb238e6
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-07 20:04:57 +0300

    init project

---
+++ berezovskoye-backend/src/main/java/org/heyjiobum/berezovskoyebackend/BerezovskoyeBackendApplication.java
@@ -0,0 +1,13 @@
+package org.heyjiobum.berezovskoyebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerezovskoyeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerezovskoyeBackendApplication.class, args);
+	}
+
+}
---
+++ berezovskoye-backend/src/test/java/org/heyjiobum/berezovskoyebackend/BerezovskoyeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.heyjiobum.berezovskoyebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerezovskoyeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
