834eb94d02ef1f8d2667ac4a731aab3cad1095aa
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-02-11 19:20:46 +0300

    add footer items

---
+++ berezovskoye-frontend/src/items/RelatedOrganizations.tsx
@@ -0,0 +1,22 @@
+export const RelatedOrganizations = () => {
+    return [
+        {
+            alt: 'БрГТУ',
+            imageSrc: '/brstu.svg',
+            width: 52.72,
+            height: 67.2,
+        },
+        {
+            alt: 'Брестоблгаз',
+            imageSrc: '/oblgaz.svg',
+            width: 67,
+            height: 67,
+        },
+        {
+            alt: 'Министерство энергетики Республики Беларусь',
+            imageSrc: '/energy_ministry.svg',
+            width: 60.31,
+            height: 67.29,
+        },
+    ];
+};
