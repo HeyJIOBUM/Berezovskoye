d5d3017bdccbd52f6eddf2516836727860830e62
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-02-11 20:04:33 +0300

    add footer to layout

--- berezovskoye-frontend/src/app/layout.tsx
+++ berezovskoye-frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import {Montserrat} from "next/font/google";
 import "./globals.css";
 import React from "react";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const montserrat = Montserrat({
     subsets: ["latin"],
@@ -17,9 +18,12 @@ export default function RootLayout({
         <html lang="en">
         <body className={`${montserrat.className}`}>
         <Header/>
-        <div className={"p-4"}>
-            {children}
+        <div className={"flex w-full items-center justify-center"}>
+            <div className="max-w-screen-lg">
+                {children}
+            </div>
         </div>
+        <Footer/>
         </body>
         </html>
     );
