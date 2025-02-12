3a1a33420501422fec05c0d6432c03c6e93a1ee5
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-02-12 19:01:15 +0300

    add image to product interface

--- berezovskoye-frontend/src/items/TestProducts.tsx
+++ berezovskoye-frontend/src/items/TestProducts.tsx
@@ -2,6 +2,7 @@ export interface Product {
     id: number;
     name: string;
     description: string;
+    imgUrl: string;
 }
 
 export const TestProducts = (): Product[] => {
@@ -10,31 +11,37 @@ export const TestProducts = (): Product[] => {
             id: 1,
             name: "Дрова ТУ BY 190799594.001-2017",
             description: "Дрова лиственных и хвойных пород деревьев.",
+            imgUrl: "/product.png",
         },
         {
             id: 2,
             name: "Дрова ТУ BY 190799594.001-2017",
             description: "Дрова лиственных и хвойных пород деревьев.",
+            imgUrl: "/product.png",
         },
         {
             id: 3,
             name: "Дрова ТУ BY 190799594.001-2017",
             description: "Дрова лиственных и хвойных пород деревьев.",
+            imgUrl: "/product.png",
         },
         {
             id: 4,
             name: "Дрова ТУ BY 190799594.001-2017",
             description: "Дрова лиственных и хвойных пород деревьев.",
+            imgUrl: "/product.png",
         },
         {
             id: 5,
             name: "Дрова ТУ BY 190799594.001-2017",
             description: "Дрова лиственных и хвойных пород деревьев.",
+            imgUrl: "/product.png",
         },
         {
             id: 6,
             name: "Дрова ТУ BY 190799594.001-2017",
             description: "Дрова лиственных и хвойных пород деревьев.",
+            imgUrl: "/product.png",
         },
     ];
 };
