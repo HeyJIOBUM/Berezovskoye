5dcec29cc9e0de3745688509b4a1758b3807188f
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-02-12 19:15:32 +0300

    move home page to products page and redirect user from home to products

--- berezovskoye-frontend/src/app/page.tsx
+++ berezovskoye-frontend/src/app/page.tsx
@@ -1,40 +1,5 @@
-"use client"
-
-import TextWithLines from "@/components/TextWithLines";
-import {Product, TestProducts} from "@/items/TestProducts";
-import ProductCard from "@/components/ProductCard";
-import Image from "next/image";
+import {redirect} from "next/navigation";
 
 export default function Home() {
-    const products = TestProducts();
-
-    return (
-        <div className="flex w-full flex-col items-center">
-            <div className="relative h-28 w-full select-none">
-                <Image
-                    src="/banner.png"
-                    fill={true}
-                    style={{objectFit: "cover"}}
-                    alt="Banner Image"
-                />
-            </div>
-            <div className="w-full max-w-screen-lg p-4">
-                <div className="flex flex-col gap-4">
-                    <TextWithLines text={"Все товары"}/>
-                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                        {products.map((product: Product) => (
-                            <ProductCard
-                                key={product.id}
-                                product={product}
-                                onAddToCart={() => {
-                                }}
-                                onDelete={() => {
-                                }}
-                            />
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+    redirect('/products');
 }
--- berezovskoye-frontend/src/app/products/page.tsx
+++ berezovskoye-frontend/src/app/products/page.tsx
@@ -1,7 +1,40 @@
+"use client"
+
+import {Product, TestProducts} from "@/items/TestProducts";
+import Image from "next/image";
+import TextWithLines from "@/components/TextWithLines";
+import ProductCard from "@/components/ProductCard";
+
 export default function Page() {
+    const products = TestProducts();
+
     return (
-        <div>
-            Products
+        <div className="flex w-full flex-col items-center">
+            <div className="relative h-28 w-full select-none">
+                <Image
+                    src="/banner.png"
+                    fill={true}
+                    style={{objectFit: "cover"}}
+                    alt="Banner Image"
+                />
+            </div>
+            <div className="w-full max-w-screen-lg p-4">
+                <div className="flex flex-col gap-4">
+                    <TextWithLines text={"Все товары"}/>
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                        {products.map((product: Product) => (
+                            <ProductCard
+                                key={product.id}
+                                product={product}
+                                onAddToCart={() => {
+                                }}
+                                onDelete={() => {
+                                }}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
