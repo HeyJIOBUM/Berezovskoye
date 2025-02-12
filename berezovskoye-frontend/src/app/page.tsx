dbb94e3b08eb6a3aa29058451db8286584c31ae2
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-02-12 19:02:45 +0300

    create main page with all products

--- berezovskoye-frontend/src/app/page.tsx
+++ berezovskoye-frontend/src/app/page.tsx
@@ -1,42 +1,40 @@
+"use client"
+
+import TextWithLines from "@/components/TextWithLines";
+import {Product, TestProducts} from "@/items/TestProducts";
+import ProductCard from "@/components/ProductCard";
+import Image from "next/image";
+
 export default function Home() {
+    const products = TestProducts();
+
     return (
-        <div className="flex flex-col gap-4">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus iaculis id arcu a luctus. Mauris
-                hendrerit vel ex eget imperdiet. Donec luctus condimentum pellentesque. Aliquam erat volutpat. Curabitur
-                pretium lacinia pretium. Nulla rhoncus pharetra diam eget egestas. Donec commodo ante tortor, ac
-                interdum metus placerat sit amet. Proin sed sapien elementum, rutrum velit ac, fermentum nisl.
-            </p>
-            <p>
-                Nam tincidunt orci vitae tellus hendrerit, quis ullamcorper diam aliquet. Suspendisse lectus lorem,
-                feugiat non lobortis in, consequat id ante. Pellentesque blandit dictum mattis. Quisque molestie varius
-                eros ac efficitur. Cras ut libero sit amet tellus accumsan semper. Mauris consectetur dignissim risus eu
-                hendrerit. Pellentesque in fermentum ante.
-            </p>
-            <p>
-                Nam arcu ante, porttitor sit amet sapien id, iaculis euismod nibh. Phasellus gravida mattis purus eu
-                venenatis. Pellentesque id ligula suscipit justo tristique fringilla vitae in purus. Sed lectus eros,
-                cursus in elit nec, fermentum viverra quam. Nullam ut auctor orci. Pellentesque habitant morbi tristique
-                senectus et netus et malesuada fames ac turpis egestas. Donec ac mauris ac lorem semper vestibulum sed
-                quis tellus. Nunc sagittis venenatis orci, in suscipit lacus mattis id. Nullam congue turpis malesuada
-                nunc consequat eleifend. Nunc nec mauris a velit fermentum lobortis. Curabitur volutpat congue elit ut
-                interdum. Praesent vitae magna dolor. Nullam id tempus dui. Donec at sem nisi. Maecenas et odio ut nunc
-                ultricies tempus et eget lectus. Etiam laoreet semper suscipit.
-            </p>
-            <p>
-                Suspendisse potenti. Suspendisse potenti. Ut quis blandit leo, at ullamcorper eros. Phasellus iaculis
-                volutpat ultrices. Aenean sollicitudin lacinia bibendum. Nam in condimentum ante. Vestibulum ultricies
-                molestie ipsum, vel ornare nisi luctus vel. Fusce dictum, ipsum sit amet suscipit finibus, est nunc
-                lobortis urna, nec mollis massa sem eget mi. Suspendisse interdum et leo at tincidunt. Vestibulum mollis
-                mauris non erat iaculis, eget aliquam mi feugiat. Mauris non sapien sodales, vehicula mauris at, semper
-                eros.
-            </p>
-            <p>
-                Vestibulum ut vestibulum elit. Vivamus dictum, lacus sed ullamcorper tristique, quam nibh ullamcorper
-                arcu, et mattis tortor tellus non dolor. Donec ullamcorper arcu orci, quis aliquam magna fringilla id.
-                Pellentesque tempor scelerisque consequat. Mauris mattis ex nisi, nec efficitur diam pretium nec. Cras
-                congue vitae mauris vel eleifend. Sed varius lacinia mi, eget convallis nibh rutrum congue.
-            </p>
+        <div className="flex w-full flex-col items-center">
+            <div className="relative h-28 w-full select-none">
+                <Image
+                    src="/banner.png"
+                    fill={true}
+                    style={{objectFit: "cover"}}
+                    alt="Banner Image"
+                />
+            </div>
+            <div className="w-full max-w-screen-lg p-4">
+                <div className="flex flex-col gap-4">
+                    <TextWithLines text={"Все товары"}/>
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                        {products.map((product: Product) => (
+                            <ProductCard
+                                key={product.id}
+                                product={product}
+                                onAddToCart={() => {
+                                }}
+                                onDelete={() => {
+                                }}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
