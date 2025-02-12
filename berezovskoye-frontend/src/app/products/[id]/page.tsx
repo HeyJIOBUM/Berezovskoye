b1782e23a4f2c059c10d28e772f74f486f918dae
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-02-13 00:16:01 +0300

    create product details page

---
+++ berezovskoye-frontend/src/app/products/[id]/page.tsx
@@ -0,0 +1,19 @@
+import TextWithLines from "@/components/TextWithLines";
+import ProductDetails from "@/components/ProductDetails";
+import {TestProducts} from "@/items/TestProducts";
+
+interface ProductPageProps {
+    params: Promise<{ id: string }>
+}
+
+export default async function Page({ params } : ProductPageProps) {
+    const productId = (await params).id;
+    const product = TestProducts()[+productId-1];
+
+    return (
+        <div className="flex w-full max-w-screen-lg flex-col gap-4 py-4">
+            <TextWithLines text={"Подробнее о товаре:"}/>
+            <ProductDetails product={product}/>
+        </div>
+    );
+}
