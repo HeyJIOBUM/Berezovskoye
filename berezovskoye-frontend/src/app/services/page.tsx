c0f3649c2892ad6833407ab54803c94ceb231e93
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-02-13 17:33:58 +0300

    create page with all services

--- berezovskoye-frontend/src/app/services/page.tsx
+++ berezovskoye-frontend/src/app/services/page.tsx
@@ -1,7 +1,22 @@
+import TextWithLines from "@/components/TextWithLines";
+import React from "react";
+import {Service, TestServices} from "@/items/TestServices";
+import ServiceCard from "@/components/ServiceCard";
+
 export default function Page() {
+    const services = TestServices();
+
     return (
-        <div>
-            Services
+        <div className="flex w-full max-w-screen-lg flex-col gap-2 py-2 sm:gap-4 sm:py-4">
+            <TextWithLines text={"Услуги"}/>
+            <div className="grid grid-cols-1 gap-2.5 md:grid-cols-2">
+                {services.map((service: Service) => (
+                    <ServiceCard
+                        key={service.id}
+                        service={service}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
---
+++ berezovskoye-frontend/src/components/ServiceCard.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import {redirect} from "next/navigation";
+import Image from "next/image";
+import React from "react";
+import {Service} from "@/items/TestServices";
+
+interface ServiceCardProps {
+    service: Service;
+}
+
+export default function ServiceCard({ service }: ServiceCardProps) {
+    const onDetailsOpen = () => {
+        redirect(`/services/${service.id}`);
+    }
+
+    const onRequestSubmit = () => {
+        console.log("отправляем заявку")
+    }
+
+    return (
+        <div className="flex flex-col items-start justify-between gap-2 bg-white p-4">
+            <div className="relative aspect-[7/3] w-full select-none">
+                <Image
+                    src={service.imgUrl}
+                    fill={true}
+                    style={{objectFit: "cover"}}
+                    alt="News Image"
+                />
+            </div>
+
+            <div className="w-full">
+                <h2 className="text-2xl">
+                    {service.title}
+                </h2>
+
+                <p className="line-clamp-3 text-base">
+                    {service.text}
+                </p>
+            </div>
+
+            <div className="flex w-full justify-between gap-2 text-sm">
+                <button
+                    className="flex-1 bg-detail p-2 font-bold text-white"
+                    onClick={onDetailsOpen}
+                >
+                    Подробнее
+                </button>
+                <button
+                    className="flex-1 bg-buy p-2 font-bold text-white"
+                    onClick={onRequestSubmit}
+                >
+                    Подать заявку
+                </button>
+            </div>
+        </div>
+    );
+}
