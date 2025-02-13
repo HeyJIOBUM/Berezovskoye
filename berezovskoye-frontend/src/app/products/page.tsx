adc085825962a513641b6f844238fdaae48fe75a
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-02-13 14:41:15 +0300

    change products gap to 2.5

--- berezovskoye-frontend/src/app/products/page.tsx
+++ berezovskoye-frontend/src/app/products/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
             <div className="w-full max-w-screen-lg py-4">
                 <div className="flex flex-col gap-4">
                     <TextWithLines text={"Все товары"}/>
-                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    <div className="grid grid-cols-1 gap-2.5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                         {products.map((product: Product) => (
                             <ProductCard
                                 key={product.id}
