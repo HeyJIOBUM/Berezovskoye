fddefa03eb7933a5807a17c4dcfedcda3a5023aa
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-02-13 16:16:32 +0300

    create page with all news

--- berezovskoye-frontend/src/app/news/page.tsx
+++ berezovskoye-frontend/src/app/news/page.tsx
@@ -1,7 +1,22 @@
+import TextWithLines from "@/components/TextWithLines";
+import React from "react";
+import {News, TestNews} from "@/items/TestNews";
+import NewsCard from "@/components/NewsCard";
+
 export default function Page() {
+    const allNews = TestNews();
+
     return (
-        <div>
-            News
+        <div className="flex w-full max-w-screen-lg flex-col gap-2 py-2 sm:gap-4 sm:py-4">
+            <TextWithLines text={"Новости"}/>
+            <div className="grid grid-cols-1 gap-2.5 md:grid-cols-2">
+                {allNews.map((news: News) => (
+                    <NewsCard
+                        key={news.id}
+                        news={news}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
