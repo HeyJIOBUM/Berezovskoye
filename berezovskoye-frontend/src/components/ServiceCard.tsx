317eaa7c6723c81573e090704ebce6d29fc344c3
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-02-13 19:12:27 +0300

    change service images and enlarge them

--- berezovskoye-frontend/src/components/ServiceCard.tsx
+++ berezovskoye-frontend/src/components/ServiceCard.tsx
@@ -20,7 +20,7 @@ export default function ServiceCard({ service }: ServiceCardProps) {
 
     return (
         <div className="flex flex-col items-start justify-between gap-2 bg-white p-2.5">
-            <div className="relative aspect-[7/3] w-full select-none">
+            <div className="relative aspect-[7/4] w-full select-none">
                 <Image
                     src={service.imgUrl}
                     fill={true}
