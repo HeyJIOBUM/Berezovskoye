32f5b3f575acf9b733ec15d95213df282d33ec12
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-02-16 22:25:46 +0300

    clamp product description, add image background

--- berezovskoye-frontend/src/components/ProductCard.tsx
+++ berezovskoye-frontend/src/components/ProductCard.tsx
@@ -12,7 +12,7 @@ interface ProductCardProps {
 export default function ProductCard({product}: ProductCardProps) {
     return (
         <div className="flex flex-col gap-1 bg-white p-1 sm:gap-2.5 sm:p-2.5">
-            <div className="relative aspect-[1] w-full select-none">
+            <div className="relative aspect-[1] w-full select-none bg-gray-300">
                 <Image
                     src={product.imgUrl}
                     fill={true}
@@ -20,9 +20,13 @@ export default function ProductCard({product}: ProductCardProps) {
                     alt="Product Image"
                 />
             </div>
-            <div>
-                <div className="text-base font-medium">{product.name}</div>
-                <p className="text-sm font-light">{product.description}</p>
+            <div className="h-full">
+                <p className="text-base font-medium">
+                    {product.name}
+                </p>
+                <p className="line-clamp-3 text-sm font-light">
+                    {product.description}
+                </p>
             </div>
             <div className="flex w-full flex-wrap justify-between gap-1 sm:gap-2">
                 <Link
