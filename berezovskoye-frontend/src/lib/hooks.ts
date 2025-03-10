6f87f0f6f67a3be2997620b3519efa0045a1bd5b
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-03-10 13:18:09 +0300

    fetch initial state from redux

--- berezovskoye-frontend/src/lib/hooks.ts
+++ berezovskoye-frontend/src/lib/hooks.ts
@@ -1,18 +1,25 @@
 "use client";
 
-import {useEffect, useState} from 'react';
+import {useEffect} from 'react';
 import {checkAuthCookie} from "@/lib/cookieUtils";
+import {useDispatch, useSelector} from "react-redux";
+import {logIn, logOut} from "@/lib/redux/authSlice";
+import {RootState} from "@/lib/redux/store";
 
 export function useAuth() {
-    const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+    const dispatch = useDispatch();
+    const isAuthInRedux = useSelector((state: RootState) => state.authReducer.isAuthenticated);
 
     useEffect(() => {
         const checkAuth = async () => {
-            setIsAuthenticated(await checkAuthCookie())
+            const isAuth = await checkAuthCookie();
+            if (isAuth != isAuthInRedux) {
+                dispatch(isAuth ? logIn() : logOut());
+            }
         };
 
         checkAuth();
-    }, []);
+    }, [dispatch, isAuthInRedux]);
 
-    return isAuthenticated;
+    return isAuthInRedux;
 }
