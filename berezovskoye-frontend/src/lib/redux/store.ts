9fcd8670fbb0ede0773df28e55ac3aac8f092784
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-03-10 13:04:23 +0300

    add auth state in redux

--- berezovskoye-frontend/src/lib/redux/store.ts
+++ berezovskoye-frontend/src/lib/redux/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import {applicationApi} from "@/lib/api/applicationApi";
+import {authReducer} from "@/lib/redux/authSlice";
 
 export const makeStore = () => {
     return configureStore({
         reducer: {
+            authReducer: authReducer,
             [applicationApi.reducerPath]: applicationApi.reducer,
         },
         middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(applicationApi.middleware),
