1de11adbecd0ef71d40c28ad745485162c77f826
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-03-13 22:22:22 +0300

    add delete button color

--- berezovskoye-frontend/tailwind.config.ts
+++ berezovskoye-frontend/tailwind.config.ts
@@ -1,24 +1,25 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        "back-bars": "var(--back-bars)",
-        "logo-color": "var(--logo-color)",
-        "lines": "var(--lines)",
-        "buy": "var(--buy)",
-        "detail": "var(--detail)",
-        "table-header": "var(--table-header)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                "back-bars": "var(--back-bars)",
+                "logo-color": "var(--logo-color)",
+                "lines": "var(--lines)",
+                "buy": "var(--buy)",
+                "detail": "var(--detail)",
+                "delete": "var(--delete)",
+                "table-header": "var(--table-header)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
