30635343050c96adb9974252dd8f71042d87eb5b
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-03-17 21:45:24 +0300

    install jwt lib, check exp in server component

--- berezovskoye-frontend/src/lib/cookieUtils.ts
+++ berezovskoye-frontend/src/lib/cookieUtils.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import {cookies} from "next/headers";
+import {jwtVerify} from "jose";
 
 export async function setAuthCookie(jwt: string) {
     const cookieStore = await cookies();
@@ -17,5 +18,26 @@ export async function checkAuthCookie(): Promise<boolean> {
     const cookieStore = await cookies();
     const jwtCookie = cookieStore.get('jwt');
 
-    return !!jwtCookie;
+    if (!jwtCookie) {
+        return false;
+    }
+
+    const jwt = jwtCookie.value;
+
+    try {
+        const secret = new TextEncoder().encode(process.env.AUTH_SECRET);
+        const {payload} = await jwtVerify(jwt, secret);
+
+        if (payload.exp) {
+            const currentTime = Math.floor(Date.now() / 1000);
+            if (payload.exp < currentTime) {
+                return false;
+            }
+        }
+
+        return true;
+    } catch (error) {
+        console.error("JWT verification failed:", error);
+        return false;
+    }
 }
