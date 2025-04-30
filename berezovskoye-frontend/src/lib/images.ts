c605ac7bb8e3adcdeb2feccdf4ab126cdd1c98f7
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-04-30 16:48:34 +0300

    refactor placeholder image

--- berezovskoye-frontend/src/lib/images.ts
+++ berezovskoye-frontend/src/lib/images.ts
@@ -1,9 +1,8 @@
 import {baseApiUrl} from "@/lib/api/applicationApi";
 
 export const getImageUrl = (path: string) => {
-    console.log(path);
     if (!path || path === "")
-        path = "/placeholder.svg"
+        return "/placeholder.svg";
     const baseUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL || baseApiUrl + '/image/';
     return `${baseUrl}${path}`;
 };
