e0e8d1791bd67e581639ed4652a63c9d244001b0
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-04-30 16:49:47 +0300

    return cookie check in server component

--- berezovskoye-frontend/src/lib/cookieUtils.ts
+++ berezovskoye-frontend/src/lib/cookieUtils.ts
@@ -1,7 +1,7 @@
 "use server"
 
 import {cookies} from "next/headers";
-//import {jwtVerify} from "jose";
+import {jwtVerify} from "jose";
 
 export async function setAuthCookie(jwt: string) {
     const cookieStore = await cookies();
@@ -14,36 +14,35 @@ export async function setAuthCookie(jwt: string) {
     })
 }
 
+export async function deleteAuthCookie(): Promise<void> {
+    const cookieStore = await cookies();
+    cookieStore.delete('jwt');
+}
 
 export async function checkAuthCookie(): Promise<boolean> {
     const cookieStore = await cookies();
-    return !!cookieStore.get('jwt');
-}
+    const jwtCookie = cookieStore.get('jwt');
 
-// export async function checkAuthCookie(): Promise<boolean> {
-//     const cookieStore = await cookies();
-//     const jwtCookie = cookieStore.get('jwt');
-//
-//     if (!jwtCookie) {
-//         return false;
-//     }
-//
-//     const jwt = jwtCookie.value;
-//
-//     try {
-//         const secret = new TextEncoder().encode(process.env.AUTH_SECRET);
-//         const {payload} = await jwtVerify(jwt, secret);
-//
-//         if (payload.exp) {
-//             const currentTime = Math.floor(Date.now() / 1000);
-//             if (payload.exp < currentTime) {
-//                 return false;
-//             }
-//         }
-//
-//         return true;
-//     } catch (error) {
-//         console.error("JWT verification failed:", error);
-//         return false;
-//     }
-// }
+    if (!jwtCookie) {
+        return false;
+    }
+
+    const jwt = jwtCookie.value;
+
+    try {
+        const secret = new TextEncoder().encode(process.env.AUTH_SECRET);
+        const {payload} = await jwtVerify(jwt, secret);
+
+        if (payload.exp) {
+            const currentTime = Math.floor(Date.now() / 1000);
+            if (payload.exp < currentTime) {
+                return false;
+            }
+        }
+
+        return true;
+    } catch (error) {
+        console.error("JWT verification failed:", error);
+        return false;
+    }
+}
